6010f6b9c94e5592e506388118368ab3032ea182
Rafael Zulianeli <rzrsantos@gmail.com>
2023-06-07 11:01:22 -0300

    feat: update global tags

--- components/GlobalTags.tsx
+++ components/GlobalTags.tsx
@@ -33,6 +33,27 @@ function GlobalTags() {
 
       {/* PrivallyApp */}
       <script type="text/javascript" id="PrivallyApp" src="https://app.privally.global/app.next.js" pid="f2b265-306786" async></script>
+
+      {/* Meta Pixel Code */}
+      <script>
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '639959938027628');
+      fbq('track', 'PageView');
+      </script>
+      <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=639959938027628&ev=PageView&noscript=1"
+      /></noscript>
+      {/* End Meta Pixel Code */}
+
+      {/* PushNews */}
+      <script src="https://cdn.pn.vg/push/pushnews-launcher.js?appId=c4c07fbe-596f-49b8-a55a-672e6e72bc2d" async></script>
     </Head>
   );
 }
